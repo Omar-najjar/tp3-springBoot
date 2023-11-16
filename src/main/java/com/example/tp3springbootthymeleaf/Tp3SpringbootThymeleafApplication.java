7e1dac80bcbd147514b0a8aad04eeb7aa862733e
Med-Omar-najjar <mohamedomar.najjar@tek-up.de>
2023-11-16 21:25:56 +0100

    first commit

---
+++ src/main/java/com/example/tp3springbootthymeleaf/Tp3SpringbootThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp3springbootthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3SpringbootThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp3SpringbootThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tp3springbootthymeleaf/Tp3SpringbootThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp3springbootthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3SpringbootThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
